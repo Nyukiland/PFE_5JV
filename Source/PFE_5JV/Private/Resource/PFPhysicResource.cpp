3bf72391850c8e82a135ee27783490b8bd32cfe2
Blanco <96238298+BlancoTLM@users.noreply.github.com>
2026-01-12 17:16:26 +0100

    [UPD] physicResource - Added first methods [NO DLLS]

--- Source/PFE_5JV/Private/Resource/PFPhysicResource.cpp
+++ Source/PFE_5JV/Private/Resource/PFPhysicResource.cpp
@@ -1,6 +1,29 @@
 #include "Resource/PFPhysicResource.h"
 
+#include "StateMachine/PFPlayerCharacter.h"
+
 float UPFPhysicResource::GetMaxSpeed() const
 {
 	return DataPtr_ ? DataPtr_->MaxSpeed : 0;
+}
+
+void UPFPhysicResource::AddForce(float ouais) const
+{
+	
+}
+
+void UPFPhysicResource::AddForce(FVector force, float duration, UCurveFloat* curve) const
+{
+	// FForceToAdd pouet(force, duration, curve);
+	// AllForces_.Add(pouet);
+}
+
+void UPFPhysicResource::SetVelocity(FVector velocity)
+{
+	Velocity_ = velocity;
+}
+
+FVector UPFPhysicResource::GetVelocity() const
+{
+	return Owner->GetVelocity();
 }
--- Source/PFE_5JV/Public/Resource/PFPhysicResource.h
+++ Source/PFE_5JV/Public/Resource/PFPhysicResource.h
@@ -1,9 +1,30 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Curves/CurveEvaluation.h"
 #include "Data/PFPhysicResourceData.h"
 #include "StateMachine/StateComponent/PFResource.h"
 #include "PFPhysicResource.generated.h"
+
+USTRUCT(Blueprintable, BlueprintType)
+struct FForceToAdd
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(editanywhere, BlueprintReadWrite)
+	FVector Force;
+	UPROPERTY(editanywhere, BlueprintReadWrite)
+	float Duration;
+	UPROPERTY(editanywhere, BlueprintReadWrite)
+	TObjectPtr<UCurveFloat> CurvePtr;
+
+	FForceToAdd(const FVector& force, const float& duration, UCurveFloat* curvePtr)
+	{
+		Force = force;
+		Duration = duration;
+		CurvePtr = curvePtr;
+	}
+};
 
 UCLASS(Abstract, Blueprintable)
 class PFE_5JV_API UPFPhysicResource : public UPFResource
@@ -13,8 +34,24 @@ class PFE_5JV_API UPFPhysicResource : public UPFResource
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PhysicResource")
 	TObjectPtr<UPFPhysicResourceData> DataPtr_;
+
+	FVector Velocity_;
+	UPROPERTY()
+	TArray<FForceToAdd> AllForces_;
 	
 public:
 	UFUNCTION(BlueprintCallable, Category = "PhysicResource")
 	float GetMaxSpeed() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PhysicResource")
+	void AddForce(float ouais) const;
+
+	UFUNCTION(BlueprintCallable, Category = "PhysicResource")
+	void AddForce(FVector force, float duration = 0, UCurveFloat* curve = nullptr) const;
+
+	UFUNCTION(BlueprintCallable, Category = "PhysicResource")
+	void SetVelocity(FVector velocity);
+
+	UFUNCTION(BlueprintCallable, Category = "PhysicResource")
+	FVector GetVelocity() const;
 };
