4b79966d62a1ba86cf5b30482e46f05f5489b59c
Nyukiland <roman211113@gmail.com>
2026-02-02 14:08:01 +0100

    [FIX] Combined state - substate update + check

--- Source/PFE_5JV/Private/StateMachine/State/PFCombinedState.cpp
+++ Source/PFE_5JV/Private/StateMachine/State/PFCombinedState.cpp
@@ -5,7 +5,10 @@ void UPFCombinedState::OnTick_Implementation(float deltaTime)
 	Super::OnTick_Implementation(deltaTime);
 
 	for (UPFState* state : SubStatePtr_)
-		state->OnEnter();
+	{
+		if (state)
+			state->OnTick(deltaTime);
+	}
 }
 
 void UPFCombinedState::OnInputTriggered_Implementation(FName inputName, ETriggerEvent triggerEvent,
@@ -14,7 +17,10 @@ void UPFCombinedState::OnInputTriggered_Implementation(FName inputName, ETrigger
 	Super::OnInputTriggered_Implementation(inputName, triggerEvent, value);
 
 	for (UPFState* state : SubStatePtr_)
-		state->OnInputTriggered(inputName, triggerEvent, value);
+	{
+		if (state)
+			state->OnInputTriggered(inputName, triggerEvent, value);
+	}
 }
 
 void UPFCombinedState::OnExit_Implementation()
@@ -22,16 +28,30 @@ void UPFCombinedState::OnExit_Implementation()
 	Super::OnExit_Implementation();
 	
 	for (UPFState* state : SubStatePtr_)
-		state->OnExit();
+	{
+		if (state)
+			state->OnExit();
+	}
 }
 
 void UPFCombinedState::AddSubState(TSubclassOf<UPFState> stateClass)
 {
+	if (!stateClass)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[CombinedState] AddSubstate subclass is null"));
+		return;
+	}
+	
 	UPFState* state = NewObject<UPFState>(this, stateClass);
+
+	if (!state)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[CombinedState] AddSubstate issue with creating class %s"), *stateClass->GetName());
+		return;
+	}
 	
 	state->Owner = Owner;
 	state->OnEnter();
 
-	
 	SubStatePtr_.Add(state);
 }
