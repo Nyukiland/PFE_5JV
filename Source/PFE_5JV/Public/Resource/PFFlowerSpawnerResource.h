4f319a379dfbf482d6ba2fd513e2620b08466834
Emilie Schott <emilie.schott@gmail.com>
2026-04-30 15:12:16 +0200

    [ADD] FlowerSpawnerResource - Base

---
+++ Source/PFE_5JV/Public/Resource/PFFlowerSpawnerResource.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateMachine/StateComponent/PFResource.h"
+#include "PFFlowerSpawnerResource.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PFE_5JV_API UPFFlowerSpawnerResource : public UPFResource
+{
+	GENERATED_BODY()
+	
+};
