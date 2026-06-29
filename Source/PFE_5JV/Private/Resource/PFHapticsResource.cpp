e99ec8e35fdbe530da0e2e6a166d9aa42d390db0
Nyukiland <roman211113@gmail.com>
2026-06-29 17:14:45 +0200

    [FIX] Haptics - issue with vibration param

--- Source/PFE_5JV/Private/Resource/PFHapticsResource.cpp
+++ Source/PFE_5JV/Private/Resource/PFHapticsResource.cpp
@@ -4,110 +4,131 @@
 
 void UPFHapticsResource::ComponentInit_Implementation(APFPlayerCharacter* ownerObj)
 {
-    Super::ComponentInit_Implementation(ownerObj);
-    PlayerController_ = Cast<APlayerController>(ownerObj->GetController());
-    CheckValidity();
+	Super::ComponentInit_Implementation(ownerObj);
+	PlayerController_ = Cast<APlayerController>(ownerObj->GetController());
+	CheckValidity();
 }
 
 void UPFHapticsResource::PlayHaptics(float intensity, float duration, FName uniqueID,
-        bool bAffectsLeftLarge, bool bAffectsLeftSmall, bool bAffectsRightLarge, bool bAffectsRightSmall)
+									bool bAffectsLarge, bool bAffectsSmall)
 {
-    if (!CheckValidity()) return;
-    
-    intensity = FMath::Clamp(intensity, 0.f, 1.f);
-    FHapticsSettings settings = FHapticsSettings(intensity, duration, 
-            bAffectsLeftLarge, bAffectsLeftSmall, bAffectsRightLarge, bAffectsRightSmall);
-    
-    if (bIsPaused_)
-    {
-        ActiveHapticsMap_.Add(uniqueID, FActiveHapticData(FDynamicForceFeedbackHandle(), settings));
-        return;
-    }
+	if (!CheckValidity()) return;
 
-    if (FActiveHapticData* existingData = ActiveHapticsMap_.Find(uniqueID))
-    {
-        UpdateHaptics(existingData, settings);
-    }
-    else
-    {
-        AddNewHaptics(uniqueID, settings);
-    }
+	intensity = FMath::Clamp(intensity, 0.f, 1.f);
+	FHapticsSettings settings = FHapticsSettings(intensity, duration,
+												bAffectsLarge, bAffectsSmall);
+
+	if (bIsPaused_)
+	{
+		ActiveHapticsMap_.Add(uniqueID, FActiveHapticData(FDynamicForceFeedbackHandle(), settings));
+		return;
+	}
+
+	if (FActiveHapticData* existingData = ActiveHapticsMap_.Find(uniqueID))
+	{
+		UpdateHaptics(existingData, settings);
+	}
+	else
+	{
+		AddNewHaptics(uniqueID, settings);
+	}
 }
 
 void UPFHapticsResource::AskToPauseHaptics()
 {
-    if (!CheckValidity() || bIsPaused_) 
-        return;
-    
-    bIsPaused_ = true;
-    
-    for (auto& Pair : ActiveHapticsMap_)
-    {
-        FActiveHapticData& Data = Pair.Value;
-        
-        PlayerController_->PlayDynamicForceFeedback(
-            0.f, 0.f, false, false, false, false, 
-            EDynamicForceFeedbackAction::Stop, 
-            Data.EngineHandle
-        );
-        
-        Data.EngineHandle = FDynamicForceFeedbackHandle(); 
-    }
+	if (!CheckValidity() || bIsPaused_)
+		return;
+
+	bIsPaused_ = true;
+
+	for (auto& Pair : ActiveHapticsMap_)
+	{
+		FActiveHapticData& Data = Pair.Value;
+
+		PlayerController_->PlayDynamicForceFeedback(
+			0.f, 0.f, false, false, false, false,
+			EDynamicForceFeedbackAction::Stop,
+			Data.EngineHandle
+		);
+
+		Data.EngineHandle = FDynamicForceFeedbackHandle();
+	}
 }
 
 void UPFHapticsResource::AskToResumeHaptics()
 {
-    if (!CheckValidity() || !bIsPaused_) 
-        return;
-    
-    bIsPaused_ = false;
-    
-    for (auto& Pair : ActiveHapticsMap_)
-    {
-        FActiveHapticData& Data = Pair.Value;
-        
-        Data.EngineHandle = PlayerController_->PlayDynamicForceFeedback(
-            Data.Settings.Intensity, Data.Settings.Duration, 
-            Data.Settings.bAffectsLeftLarge, Data.Settings.bAffectsLeftSmall, 
-            Data.Settings.bAffectsRightLarge, Data.Settings.bAffectsRightSmall,
-            EDynamicForceFeedbackAction::Start, 
-            FDynamicForceFeedbackHandle() 
-        );
-    }
+	if (!CheckValidity() || !bIsPaused_)
+		return;
+
+	bIsPaused_ = false;
+
+	for (auto& Pair : ActiveHapticsMap_)
+	{
+		FActiveHapticData& Data = Pair.Value;
+
+		Data.EngineHandle = PlayerController_->PlayDynamicForceFeedback(
+			Data.Settings.Intensity, Data.Settings.Duration,
+			Data.Settings.bAffectsLarge, Data.Settings.bAffectsSmall,
+			Data.Settings.bAffectsLarge, Data.Settings.bAffectsSmall,
+			EDynamicForceFeedbackAction::Start,
+			FDynamicForceFeedbackHandle()
+		);
+	}
 }
 
 void UPFHapticsResource::AddNewHaptics(FName uniqueID, const FHapticsSettings& settings)
 {
-    FDynamicForceFeedbackHandle NewHandle = PlayerController_->PlayDynamicForceFeedback(
-            settings.Intensity, settings.Duration, 
-            settings.bAffectsLeftLarge, settings.bAffectsLeftSmall, 
-            settings.bAffectsRightLarge, settings.bAffectsRightSmall,
-            EDynamicForceFeedbackAction::Start, 
-            FDynamicForceFeedbackHandle()
-    );
+	FDynamicForceFeedbackHandle NewHandle = PlayerController_->PlayDynamicForceFeedback(
+		settings.Intensity, settings.Duration,
+		settings.bAffectsLarge, settings.bAffectsSmall,
+		settings.bAffectsLarge, settings.bAffectsSmall,
+		EDynamicForceFeedbackAction::Start,
+		FDynamicForceFeedbackHandle()
+	);
 
-    ActiveHapticsMap_.Add(uniqueID, FActiveHapticData(NewHandle, settings));
+	ActiveHapticsMap_.Add(uniqueID, FActiveHapticData(NewHandle, settings));
 }
 
 void UPFHapticsResource::UpdateHaptics(FActiveHapticData* existingData, const FHapticsSettings& settings)
 {
-    existingData->Settings = settings;
+	existingData->Settings = settings;
+
+	if (settings.Duration == -1)
+	{
+		PlayerController_->PlayDynamicForceFeedback(
+			settings.Intensity, settings.Duration,
+			settings.bAffectsLarge, settings.bAffectsSmall,
+			settings.bAffectsLarge, settings.bAffectsSmall,
+			EDynamicForceFeedbackAction::Update,
+			existingData->EngineHandle
+		);
+		
+		return;
+	}
+	
+	PlayerController_->PlayDynamicForceFeedback(
+		0.f, 0.f, false, false, false, false, 
+		EDynamicForceFeedbackAction::Stop, 
+		existingData->EngineHandle
+	);
+
+	existingData->Settings = settings;
         
-    PlayerController_->PlayDynamicForceFeedback(
-        settings.Intensity, settings.Duration, 
-        settings.bAffectsLeftLarge, settings.bAffectsLeftSmall, 
-        settings.bAffectsRightLarge, settings.bAffectsRightSmall,
-        EDynamicForceFeedbackAction::Update, 
-        existingData->EngineHandle
-    );
+	existingData->EngineHandle = PlayerController_->PlayDynamicForceFeedback(
+		settings.Intensity, settings.Duration, 
+		settings.bAffectsLarge, settings.bAffectsSmall, 
+		settings.bAffectsLarge, settings.bAffectsSmall,
+		EDynamicForceFeedbackAction::Start, 
+		FDynamicForceFeedbackHandle()
+	);
 }
 
 bool UPFHapticsResource::CheckValidity()
 {
-    if (!PlayerController_)
-    {
-       UE_LOG(LogTemp, Error, TEXT("[HapticsResource] Failed to get the PlayerController"))
-       return false;
-    }
-    return true;
+	if (!PlayerController_)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[HapticsResource] Failed to get the PlayerController"))
+		return false;
+	}
+	return true;
 }
