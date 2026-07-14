62af3c4d2eeccec096f8557d2f79405d51374483
Nyukiland <roman211113@gmail.com>
2026-07-14 21:45:59 +0200

    [ADD] Shader - ush folder

--- Source/PFE_5JV/PFE_5JV.cpp
+++ Source/PFE_5JV/PFE_5JV.cpp
@@ -1,6 +1,20 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "PFE_5JV.h"
 #include "Modules/ModuleManager.h"
+#include "RenderUtils.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PFE_5JV, "PFE_5JV" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FPFE_5JVModule, PFE_5JV, "PFE_5JV" );
+
+void FPFE_5JVModule::StartupModule()
+{
+	FString ShaderDir = FPaths::Combine(FPaths::ProjectDir(), TEXT("Shaders"));
+    
+	if (FPaths::DirectoryExists(ShaderDir))
+	{
+		AddShaderSourceDirectoryMapping(TEXT("/PFE_5JV"), ShaderDir);
+	}
+}
+
+void FPFE_5JVModule::ShutdownModule()
+{
+	
+}
