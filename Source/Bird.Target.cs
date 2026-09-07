415eb1ba79449d362f76d93ca323bad9adafaa86
Nyukiland <roman211113@gmail.com>
2026-09-07 22:52:39 +0200

    [UPD] supposed to be better

--- Source/Bird.Target.cs
+++ Source/Bird.Target.cs
@@ -3,9 +3,9 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class PFE_5JVTarget : TargetRules
+public class BirdTarget : TargetRules
 {
-	public PFE_5JVTarget(TargetInfo Target) : base(Target)
+	public BirdTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
